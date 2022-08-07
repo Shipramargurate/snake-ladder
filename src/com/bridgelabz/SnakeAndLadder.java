aed58341a811b2f81c1c9dcc97bb3c1542d42efe
shipramargurate <shivanikumari22322@gmail.com>
2022-08-07 20:31:43 +0530

    [shivani]:check position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 
+
 	public static void main(String[] args) {
 		/*
 		 * UC1- start from position 0
@@ -14,5 +15,26 @@ System.out.println("position of player 1 is:" + position );
  */
 int roll = (int)(Math.random()*6)+1;
 System.out.println("after rolling dice:" + roll );
+/*
+ * UC3- no play, snake aand ladder
+ */
+int options = (int)(Math.random() * 100 ) % 3;
+System.out.println("option = " + options);
+switch (options) {
+case 1:
+	System.out.println("ladder = moving ahead...!!");
+	position += roll;
+	break;
+case 2:
+	System.out.println("snake = going back...!!");
+	position -= roll;
+	if (position < 0)
+		position = 0;
+	break;
+	default :
+		System.out.println("safer side...!!");
+}
+System.out.println("position of single player iis = " + position);
 	}
+
 }

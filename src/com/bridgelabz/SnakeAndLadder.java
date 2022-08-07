6f17dab84c100bb08aa287012b46a1bd01e0dd12
shipramargurate <shivanikumari22322@gmail.com>
2022-08-07 20:28:37 +0530

    [shivani]: roll the dice

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,14 +1,18 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+
 	public static void main(String[] args) {
 		/*
 		 * UC1- start from position 0
 		 */
 		int position = 0;
-System.out.println("welcome to snake and ladder game program");
-System.out.println("position of player 1 is:" + position);
-
+System.out.println("welcome to snake and ladder game");
+System.out.println("position of player 1 is:" + position );
+/*
+ * UC2- roll the dice
+ */
+int roll = (int)(Math.random()*6)+1;
+System.out.println("after rolling dice:" + roll );
 	}
-
 }
